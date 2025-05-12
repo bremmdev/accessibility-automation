03662ebc1358d072ed740cdbdbec7e6c9f6a19b5
bremmdev <bremmdev@gmail.com>
2025-05-12 23:20:47 +0200

    cleanup code, move multiple functions in same try catch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import puppeteer from '@cloudflare/puppeteer';
-import { formatAccessibilityResults, isValidUrl } from './utils';
+import puppeteer, { Browser } from '@cloudflare/puppeteer';
+import { formatAccessibilityResults, isValidUrl, setupPage } from './utils';
 
 interface Env {
 	MYBROWSER: Fetcher;
@@ -8,6 +8,7 @@ interface Env {
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const axeScriptUrl = 'https://cdnjs.cloudflare.com/ajax/libs/axe-core/4.10.3/axe.min.js';
+
 		const { searchParams } = new URL(request.url);
 		let targetUrl = searchParams.get('url');
 
@@ -20,53 +21,34 @@ export default {
 		}
 
 		const browser = await puppeteer.launch(env.MYBROWSER);
-		const page = await browser.newPage();
+		const page = await setupPage(browser);
 
-		// Bypass Content Security Policy (CSP) for the page, often necessary to allow injected scripts like axe-core to run
-		await page.setBypassCSP(true);
+		let accessibilityResults;
 
+		// go to url and inject axe-core script and wait for it to load
 		try {
 			await page.goto(targetUrl, {
 				waitUntil: 'networkidle0', // Wait until network activity is idle, indicating the page has loaded
 				timeout: 10000,
 			});
-		} catch (e: any) {
-			console.error(`Error navigating to ${targetUrl}: ${e.message}`);
-			if (browser) await browser.close();
-			return new Response(`Failed to navigate to URL: ${e.message}`, {
-				status: 500,
-			});
-		}
-
-		// inject the axe-core script into the page
-		try {
 			await page.addScriptTag({ url: axeScriptUrl });
+			await page.waitForFunction('window.axe !== undefined', {
+				timeout: 15000,
+			});
+			accessibilityResults = await page.evaluate(async () => {
+				// window.axe.configure({ })
+
+				// @ts-ignore // Tell TypeScript to ignore that 'axe' is not defined in Worker's scope
+				return await window.axe.run();
+				// To scan specific parts of the page, you can pass a context:
+				// return await window.axe.run(document.body, { /* options */ });
+			});
 		} catch (e: any) {
 			if (browser) await browser.close();
-			return new Response(`Failed to inject axe-core: ${e.message}`, {
+			return new Response(`Error during execution: ${e.message}`, {
 				status: 500,
 			});
 		}
-
-		// Wait for the `axe` object to be defined on the window object in the page's context.
-		try {
-			await page.waitForFunction('window.axe !== undefined', {
-				timeout: 15000, // 15-second timeout
-			});
-		} catch (e: any) {
-			if (browser) await browser.close();
-			return new Response('axe-core script did not load correctly in the page context.', { status: 500 });
-		}
-
-		const accessibilityResults = await page.evaluate(async () => {
-			// You can configure axe before running if needed:
-			// window.axe.configure({ rules: [ { id: 'color-contrast', enabled: true } ] });
-
-			// @ts-ignore // Tell TypeScript to ignore that 'axe' is not defined in Worker's scope
-			return await window.axe.run();
-			// To scan specific parts of the page, you can pass a context:
-			// return await window.axe.run(document.body, { /* options */ });
-		});
 
 		return new Response(JSON.stringify(formatAccessibilityResults(accessibilityResults), null, 2), {
 			headers: { 'Content-Type': 'application/json' },
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Browser } from '@cloudflare/puppeteer';
+
 export function isValidUrl(url: string | null): boolean {
 	if (!url) {
 		return false;
@@ -30,4 +32,22 @@ export function formatAccessibilityResults(results: any) {
 	}
 
 	return formattedResults;
+}
+
+// Setup a new page with specific user agent and viewport settings to simulate a desktop browser
+export async function setupPage(browser: Browser) {
+	const page = await browser.newPage();
+	page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36');
+	page.setViewport({
+		width: 1920,
+		height: 1080,
+		deviceScaleFactor: 1,
+		isMobile: false,
+		hasTouch: false,
+		isLandscape: true,
+	});
+
+	// Bypass Content Security Policy (CSP) for the page, often necessary to allow injected scripts like axe-core to run
+	await page.setBypassCSP(true);
+	return page;
 }
